427cd05e174c76cda204cba94df677da830df2d1
S Davis <myriagon@sddw.net>
2017-10-01 23:33:42 -0600

    Initial check-in.  This corresponds to my-types.h.1

---
+++ src/my-types.h
@@ -0,0 +1,10 @@
+// Platform-dependent types
+typedef char  sint8;
+typedef short sint16;
+typedef int   sint32;
+typedef long  sint64;
+typedef unsigned char  uint8;
+typedef unsigned short uint16;
+typedef unsigned int   uint32;
+typedef unsigned long  uint64;
+
