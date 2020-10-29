9e692eea32bb471369c5d48282537184abfd5eb2
S Davis <myriagon@sddw.net>
2020-10-29 11:28:38 -0700

    Update copyright and add Apache 2.0 license

--- src/hodge-podge.c
+++ src/hodge-podge.c
@@ -6,7 +6,21 @@
    structures in chemical systems.  Gerhardt M, Schuster H.
    Physica D 36 (1989) 209-221
 
-   Copyright 2017 S Davis
+   Copyright (c) 2017-2020 Scott Davis
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+   Please acknowledge my work if it is useful to you.
 */
 
 
